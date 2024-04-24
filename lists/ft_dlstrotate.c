12c7577b5a68940520dc24e04b0a2a2a55f0e88e
Jonathan Eberle <jeberle@2-D-5.42heilbronn.de>
2024-04-24 11:17:31 +0200

    debug rotation

--- lists/ft_dlstrotate.c
+++ lists/ft_dlstrotate.c
@@ -6,7 +6,7 @@
 /*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 20:57:19 by jonathanebe       #+#    #+#             */
-/*   Updated: 2024/04/24 11:11:54 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/04/24 11:17:24 by jeberle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void    ft_dlstrotateleft(t_dlist **lst, int n)
 {
+	ft_putstr("ft_dlstrotateleft\n");
 	t_dlist *head;
 	t_dlist *tail;
 
@@ -34,6 +35,7 @@ void    ft_dlstrotateleft(t_dlist **lst, int n)
 }
 void    ft_dlstrotateright(t_dlist **lst, int n)
 {
+	ft_putstr("ft_dlstrotateright\n");
 	t_dlist *head;
 	t_dlist *tail;
 
@@ -54,6 +56,7 @@ void    ft_dlstrotate(t_dlist **lst, int n, int direction)
 {
 	int len;
 
+	ft_putstr("ft_dlstrotate\n");
     if (!lst || !*lst || n == 0)
         return;
 
