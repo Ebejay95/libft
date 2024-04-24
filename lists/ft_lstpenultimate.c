1cf67f1603d23966ce7d4bedea45bf0e49ca8b58
Jonathan Eberle <jeberle@2-D-1.42heilbronn.de>
2024-04-25 01:43:12 +0200

    norm corrections

--- lists/ft_lstpenultimate.c
+++ lists/ft_lstpenultimate.c
@@ -6,7 +6,7 @@
 /*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 01:05:07 by jeberle           #+#    #+#             */
-/*   Updated: 2024/04/25 01:31:16 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/04/25 01:39:08 by jeberle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 t_list	*ft_lstpenultimate(t_list *lst)
 {
 	if (lst == NULL || lst->next == NULL)
-		return (NULL);	
+		return (NULL);
 	while (lst->next->next != NULL)
 		lst = lst->next;
 	return (lst);
--- lists/ft_lstrotate.c
+++ lists/ft_lstrotate.c
@@ -6,29 +6,50 @@
 /*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 15:00:33 by jonathanebe       #+#    #+#             */
-/*   Updated: 2024/04/25 01:20:12 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/04/25 01:42:50 by jeberle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../libft.h"
 
-void	ft_lstrotate(t_list **lst, int n, int direction)
+static void	swap_front_to_back(t_list **lst)
+{
+	t_list	*front;
+	t_list	*back;
+
+	front = (*lst);
+	back = ft_lstlast((*lst));
+	back->next = front;
+	(*lst) = (*lst)->next;
+	front->next = NULL;
+}
+
+static void	swap_back_to_front(t_list **lst)
 {
 	t_list	*front;
 	t_list	*back;
 	t_list	*penultimate;
 
+	front = (*lst);
+	penultimate = ft_lstpenultimate((*lst));
+	if (penultimate == NULL || penultimate->next == NULL)
+		return ;
+	back = penultimate;
+	front = penultimate->next;
+	back->next = NULL;
+	front->next = (*lst);
+	(*lst) = front;
+}
+
+void	ft_lstrotate(t_list **lst, int n, int direction)
+{
 	if (lst == NULL || (*lst) == NULL || (*lst)->next == NULL)
 		return ;
 	if (direction > 0)
 	{
 		while (n > 0)
 		{
-			front = (*lst);
-			back = ft_lstlast((*lst));
-			back->next = front;
-			(*lst) = (*lst)->next;
-			front->next = NULL;
+			swap_front_to_back(lst);
 			n--;
 		}
 	}
@@ -36,15 +57,7 @@ void	ft_lstrotate(t_list **lst, int n, int direction)
 	{
 		while (n > 0)
 		{
-			front = (*lst);
-			penultimate = ft_lstpenultimate((*lst));
-			if(penultimate == NULL || penultimate->next == NULL)
-				return ;
-			back = penultimate;
-			front = penultimate->next;
-			back->next = NULL;
-			front->next = (*lst);
-			(*lst) = front;
+			swap_back_to_front(lst);
 			n--;
 		}
 	}
