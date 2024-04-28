c405ecf1bc39bce18f70796c1fb5b30e08e5417f
Jonathan Eberle <jonathaneberle@MAC42.fritz.box>
2024-04-28 16:07:56 +0200

    correction in kgv

--- math/ft_kgv.c
+++ math/ft_kgv.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_kgv.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jonathaneberle <jonathaneberle@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 08:54:06 by jeberle           #+#    #+#             */
-/*   Updated: 2024/04/18 09:13:34 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/04/28 16:07:25 by jonathanebe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,6 @@
 
 int	ft_kgv(int a, int b)
 {
-	int n;
-
-	n = 0;
 	if ((a == 0) || (b == 0))
 		return (0);
 	return ((a / ft_ggt(a,b)) * b);
