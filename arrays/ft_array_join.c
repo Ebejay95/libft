a2083615c2855d9c358c9ae10e8c41c92f22adb7
Jonathan Eberle <jeberle@2-B-4.42heilbronn.de>
2024-05-19 20:32:38 +0200

    array join beginning

---
+++ arrays/ft_array_join.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_array_join.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/19 18:48:44 by jeberle           #+#    #+#             */
+/*   Updated: 2024/05/19 18:51:22 by jeberle          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./../libft.h"
+
+char	*ft_array_join(char **array)
+{
+	int		length;
+	size_t	i;
+
+	i = 0;
+	if (array == NULL)
+		return ;
+	length = ft_array_length(array);
+	if (array == NULL)
+		return ;
+	while (i < len)
+	{
+		i++;
+	}
+}
