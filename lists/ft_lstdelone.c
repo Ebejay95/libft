08e2800cb9a352f1ce953d642c8c01119bf5db18
Jonathan Eberle <jeberle@student.42heilbronn.de>
2024-08-12 21:07:27 +0200

    lstclearfree

--- lists/ft_lstdelone.c
+++ lists/ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 12:56:36 by jeberle           #+#    #+#             */
-/*   Updated: 2024/06/19 10:51:36 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/08/12 21:06:53 by jeberle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 	if (lst == NULL || del == NULL)
 		return ;
 	del(lst->content);
+	free(lst->content);
 	free(lst);
 }
